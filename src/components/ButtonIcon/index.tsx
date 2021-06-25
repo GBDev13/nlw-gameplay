a3a10e8fa3b36d23c33d3e50ed051e40abeda5e4
Gabriel Borges <gbdesigns13@gmail.com>
2021-06-25 15:35:05 -0300

    Aula 01 Finalizada!

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Text,
+  Image,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps
+} from 'react-native';
+
+import DiscordImg from '../../assets/discord.png';
+import { styles } from './styles';
+
+type Props = TouchableOpacityProps & {
+  title: string;
+}
+
+export function ButtonIcon({ title, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.7} {...rest} >
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon}/>
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  StatusBar
+} from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { styles } from './styles';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se{'\n'}
+          e organize suas{'\n'}
+          jogatinas
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{'\n'}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon title="Entrar com Discord" />
+      </View>
+    </View>
+  )
+}
